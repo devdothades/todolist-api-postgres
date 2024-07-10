6a092be437ed7f03f365a9e9d28a52f6d2d8741e
Jerome A. Infante <jeromeinfante0506@gmail.com>
2024-07-10 10:39:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import pool from "./model/schema.js";
+dotenv.configDotenv();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+pool.connect()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(
+                `Server is running on port ${process.env.PORT} and connected to the ${process.env.DB_NAME} database`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
---
+++ model/schema.js
@@ -0,0 +1,42 @@
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.configDotenv();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+const createTables = async () => {
+    const queryText = `CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+    )`;
+
+    const queryText2 = `CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+        task VARCHAR(128) UNIQUE NOT NULL,
+        status BOOLEAN DEFAULT FALSE NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW(),
+        user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+    )`;
+
+    try {
+        const res = await pool.query(queryText2);
+        console.log(res);
+        console.log("Tables created successfully");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// createTables();
+
+export default pool;
