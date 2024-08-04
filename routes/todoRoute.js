84b0dc37758653da2329f8c7f211783dcb57005c
Jerome A. Infante <jeromeinfante0506@gmail.com>
2024-08-04 08:55:12 +0800

    crud done

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -10,13 +10,13 @@ import { validate, taskValidationRules } from "../middleware/validation.js";
 
 const router = Router();
 
-router.use(taskValidationRules);
-router.use(validate);
+// router.use(taskValidationRules);
+// router.use(validate);
 router.use(authenticateToken);
 
-router.post("/add", addTask);
-router.get("/add", getTask);
-router.patch("/add", updateTask);
-router.delete("/add", deleteTask);
+router.post("/add", taskValidationRules, validate, addTask);
+router.get("/get", getTask);
+router.patch("/update/:id", taskValidationRules, validate, updateTask);
+router.delete("/delete/:id", deleteTask);
 
 export default router;
